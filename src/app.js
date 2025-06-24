c34550ea330b1cb6da1eae13c6fe4392d273f289
Candela991111 <candexvidela@gmail.com>
2025-06-24 07:55:18 +0000

    cambios finales

--- src/app.js
+++ src/app.js
@@ -10,12 +10,16 @@ window.onload = function () {
   console.log("Hello Rigo from the console!");
 
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-let randoWho = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)]
+  }
 
-document.getElementById('excuse').innerHTML = randoWho;
+  let randoWho= getRandomElement (who)+ getRandomElement (action)+ getRandomElement (what) + getRandomElement (when) 
+
+  document.getElementById('excuse').innerHTML = randoWho;
 };
